c58a5bcd334b77f342cbc955d45304fb912032d5
Rick Moore <rem63@cornell.edu>
2014-12-05 17:36:15 -0500

    v 1.1.0 additional functionality in acis, ag, stations, utils, added hdf5 file read/write functions

--- atmosci/acis/stndata.py
+++ atmosci/acis/stndata.py
@@ -5,21 +5,20 @@ from .client import AcisWebServicesClient
 
 class AcisStationDataClient(AcisWebServicesClient):
 
-    def request(self, **kwargs):
-        return AcisWebServicesClient.request(self, 'StnData', **kwargs)
+    def request(self, **request_dict):
+        return self.submitRequest( 'StnData', **request_dict)
 
     def query(self, json_query_string):
-        return AcisWebServicesClient.query(self, 'StnData', json_query_string)
+        return self.submitQuery('StnData', json_query_string)
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 class AcisMultiStationDataClient(AcisWebServicesClient):
     
-    def request(self, **kwargs):
-        return AcisWebServicesClient.request(self, 'MultiStnData', **kwargs)
+    def request(self, **request_dict):
+        return self.submitRequest( 'MultiStnData', **request_dict)
 
     def query(self, json_query_string):
-        return AcisWebServicesClient.query(self, 'MultiStnData',
-                                           json_query_string)
+        return self.submitQuery('MultiStnData', json_query_string)
 
